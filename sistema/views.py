b464a37340c742ad15ad56a29e5bcd7ccfa4f67e
junior <juniorsa@gmail.com>
2025-03-17 16:38:41 -0300

    fazendo caminhos/fragmentaçao

--- sistema/views.py
+++ sistema/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+# aqui irao ficar todas as views controladores referente ao sistema
+
+def index(request): #funçao esperando uma requisiçao
+    return render(
+        request,
+        'sistema/index.html',
+    )
+
+
