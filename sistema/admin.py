d65b993abf542d3b3210ca7e69d10c81c1c0e2d2
junior <juniorsa@gmail.com>
2025-03-28 16:27:20 -0300

    models/admin

--- sistema/admin.py
+++ sistema/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
 
 from sistema import models
-
-# aqui fica o resgistro do usuário
-
+# aqui fica o registro do usuario
 @admin.register(models.Usuario)
 class UsuarioAdmin(admin.ModelAdmin):
     list_display = ('id', 'nome', 'sobrenome', 'email', 'ativo',)
+
+# aqu fica o registro do filme
+@admin.register(models.Filme)
+class FilmeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'ano', 'studio', 'genero',)
+
+@admin.register(models.Genero)
+class GeneroAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome',)
--- sistema/models.py
+++ sistema/models.py
@@ -10,9 +10,31 @@ class Usuario(models.Model):
     nome = models.CharField(max_length=50)
     sobrenome = models.CharField(max_length=50)
     cpf = models.CharField(max_length=11)
-    telefone = models.CharField(max_lengJth=20)
+    telefone = models.CharField(max_length=20)
     email = models.EmailField()
     endereco = models.CharField(max_length=100)
     data_cadastro = models.DateTimeField(default=timezone.now)
     ativo = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f'{self.nome} {self.sobrenome}'
+
+class Filme(models.Model):
+    nome =  models.CharField(max_length=50)
+    ano = models.DateField(default=timezone.now)
+    studio = models.CharField(max_length=50)
+    genero = models.CharField(max_length=15)
+    sinopse = models.TextField()
+    data_cadastro = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.nome
+
+class Genero(models.Model):
+    nome =  models.CharField(max_length=15)
+    data_cadastro = models.DateTimeField(default=timezone.now)
+    
+    def __str__(self):
+        return self.nome
+
     
